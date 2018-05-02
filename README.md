d6797f8dee44518c5eea7cf5648b8cad80eb586e
kyahra <sofiacbahamonde@gmail.com>
2018-05-02 18:09:19 +0100

    sll socket

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SDIS_Proj2
+
+java -Djavax.net.ssl.trustStore=truststore -Djavax.net.ssl.trustStorePassword=123456 -Djavax.net.ssl.keyStore=server.keys -Djavax.net.ssl.keyStorePassword=123456 SSLServer
+
+java -Djavax.net.ssl.trustStore=truststore -Djavax.net.ssl.trustStorePassword=123456 -Djavax.net.ssl.keyStore=client.keys -Djavax.net.ssl.keyStorePassword=123456 SSLClient
---
+++ src/SSLClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import javax.net.ssl.*;
+
+public class SSLClient {
+
+  private static final String HOST = "localhost";
+
+  private static final int PORT = 8080;
+
+  public static void main(String[] args) throws Exception {
+    SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    SSLSocket s = (SSLSocket) sf.createSocket(HOST, PORT);
+    s.setEnabledCipherSuites(sf.getSupportedCipherSuites());
+    
+    OutputStream out = s.getOutputStream();
+    out.write("\nConnection established.\n\n".getBytes());
+    out.flush();
+    int theCharacter = 0;
+    theCharacter = System.in.read();
+    
+    while (theCharacter != '~') // The '~' is an escape character to exit
+    {
+      out.write(theCharacter);
+      out.flush();
+      theCharacter = System.in.read();
+    }
+
+    out.close();
+    s.close();
+  }
+}
