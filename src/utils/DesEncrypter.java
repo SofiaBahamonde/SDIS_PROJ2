5302f576fad37a9810dcb915fc9e51c4fc459b9c
TrocaTudo95 <pedromachado_95@sapo.pt>
2018-05-19 16:36:01 +0100

    encrypted messages

---
+++ src/utils/DesEncrypter.java
@@ -0,0 +1,65 @@
+package utils;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+  
+public class DesEncrypter {
+  
+    Cipher ecipher;
+    Cipher dcipher;
+  
+    public DesEncrypter(SecretKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        
+            ecipher = Cipher.getInstance("AES");
+            System.out.println("ola");
+            dcipher = Cipher.getInstance("AES");
+            System.out.println("ola");
+            ecipher.init(Cipher.ENCRYPT_MODE, key);
+            System.out.println("ola");
+            dcipher.init(Cipher.DECRYPT_MODE, key);
+            System.out.println("ola");
+       
+    }
+    
+  
+    public String encrypt(String str) {
+        try {
+            // Encode the string into bytes using utf-8
+            byte[] utf8 = str.getBytes("UTF8");
+  
+            // Encrypt
+            byte[] enc = ecipher.doFinal(utf8);
+  
+            // Encode bytes to base64 to get a string
+            return new sun.misc.BASE64Encoder().encode(enc);
+        } catch (javax.crypto.BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        } catch (UnsupportedEncodingException e) {
+        } catch (java.io.IOException e) {
+        }
+        return null;
+    }
+  
+    public String decrypt(String str) {
+        try {
+            // Decode base64 to get bytes
+            byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+            // Decrypt
+            byte[] utf8 = dcipher.doFinal(dec);
+            // Decode using utf-8
+            return new String(utf8, "UTF8");
+        } catch (javax.crypto.BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        } catch (UnsupportedEncodingException e) {
+        } catch (java.io.IOException e) {
+        }
+        return null;
+    }
+}
---
+++ src/utils/SecretKeyGenerator.java
@@ -0,0 +1,62 @@
+package utils;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class SecretKeyGenerator {
+
+ public static SecretKey generateSecretKey() {
+  KeyGenerator keyGen = null;
+  try {
+   /*
+    * Get KeyGenerator object that generates secret keys for the
+    * specified algorithm.
+    */
+   keyGen = KeyGenerator.getInstance("AES");
+  } catch (NoSuchAlgorithmException e) {
+   e.printStackTrace();
+  }
+
+  /* Initializes this key generator for key size to 256. */
+  keyGen.init(256);
+
+  /* Generates a secret key */
+  SecretKey secretKey = keyGen.generateKey();
+
+  return secretKey;
+ }
+
+ 
+ //converts the secretKey to a string to be more easy to send the message
+ public static String keyToString(SecretKey secretKey) {
+  /* Get key in encoding format */
+  byte encoded[] = secretKey.getEncoded();
+
+  /*
+   * Encodes the specified byte array into a String using Base64 encoding
+   * scheme
+   */
+  String encodedKey = Base64.getEncoder().encodeToString(encoded);
+
+  return encodedKey;
+ }
+
+ 
+ //converts the keyString to SecretKey
+ public static SecretKey decodeKeyFromString(String keyStr) {
+  /* Decodes a Base64 encoded String into a byte array */
+  byte[] decodedKey = Base64.getDecoder().decode(keyStr);
+
+  /* Constructs a secret key from the given byte array */
+  SecretKey secretKey = new SecretKeySpec(decodedKey, 0,
+    decodedKey.length, "AES");
+
+  return secretKey;
+ }
+
+
+}
